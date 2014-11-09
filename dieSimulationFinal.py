4ec14e1abab080e6ab845ddf4e6fbedc4eb01eae
Tommy Lin <tommylin1998@outlook.com>
2014-11-09 16:29:35 -0500

    New feature

--- dieSimulationFinal.py
+++ dieSimulationFinal.py
@@ -2,12 +2,9 @@
 
 import random
 
-<<<<<<< HEAD
-=======
 dice = int(raw_input('How many dice would you like to roll? '))
->>>>>>> askDice
-sides = int(raw_input('How many sides on your die? '))
 
-for dice in range(0, sides + 1):
+for dice in range(0, dice):
+    sides = int(raw_input('How many sides on your die? #{}'.format(dice)))
     number = random.randint(1, sides)
     print('The die shows: {}'.format(number))
